c881749039cb3f0a02fd62dd5e2d5ac2c2d4ac95
Anthony Chablov <achablov@myseneca.ca>
2024-12-15 17:16:50 -0500

    feat: add hover effects and contact icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,113 +3,11 @@ import HeaderText from "@/components/Text/HeaderText";
 import Separator from "@/components/Utils/Separator";
 import ParagraphText from "@/components/Text/ParagraphText";
 import DitheredBox from "@/components/AnimatedBanners/DitheredBanner";
-
-const skills = [
-  "JavaScript, TypeScript",
-  "Express.js, Node.js",
-  "React.js, Next.js, Remix.js, React Meta-Frameworks",
-  "Solidity, Hardhat, Foundry",
-  "Leaflet.js, D3.js",
-  "CSS/SCSS, Tailwind CSS, MUI",
-];
-
-const interests = [
-  "Full Stack Web Development",
-  "Decentralized & Distributed Systems",
-  "System Design",
-  "Software Architecture",
-  "User Experience",
-  "User Interface Design",
-];
-
-const hobbies = [
-  {
-    title: "Rock Climbing + Bouldering",
-    content:
-      "Passionate about bouldering and rock-climbing. I enjoy pushing the limits, improving my technique, and exploring outdoor climbing spots.",
-  },
-  {
-    title: "Music Production",
-    content:
-      "Creating and mixing tracks using digital audio workstations like Ableton Live and Fl Studio.",
-  },
-  {
-    title: "Geographic Information Systems",
-    content:
-      "Fascinated by mapping, spatial data analysis and using GIS tools like QGIS to interpret, visualize, and analyze geospatial data.",
-  },
-  {
-    title: "Design",
-    content:
-      "Creating and experimenting with user-centric designs that are both visually appealing and functionally effective.",
-  },
-];
-const contactLinks = [
-  {
-    href: "https://www.linkedin.com/in/your-profile",
-    text: "LinkedIn: linkedin.com/in/your-profile",
-    icon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        className="w-6 h-6"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          d="M4 4h16M4 4v16m16-16v16"
-        />
-      </svg>
-    ),
-  },
-  {
-    href: "https://github.com/your-username",
-    text: "GitHub: github.com/your-username",
-    icon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        className="w-6 h-6"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm1-12h-2v4H9l3 3 3-3h-2z"
-        />
-      </svg>
-    ),
-  },
-  {
-    href: "https://leetcode.com/your-username",
-    text: "LeetCode: leetcode.com/your-username",
-    icon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        className="w-6 h-6"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          d="M5 12h14M12 5l7 7-7 7"
-        />
-      </svg>
-    ),
-  },
-];
+import { skills, interests, hobbies, contactLinks } from "./profileData";
 
 export default function Home() {
   return (
-    <main className="h-full  bg-zinc-900 ">
+    <main className="h-full  bg-black ">
       <DitheredBox width="100%" />
       <Container>
         <section>
@@ -209,15 +107,20 @@ export default function Home() {
           <Separator />
           <ul className="space-y-4 mt-4">
             {contactLinks.map((link, index) => (
-              <li key={index} className="flex items-center space-x-2">
+              <li key={index} className="flex items-center space-x-2 group">
                 <a
                   href={link.href}
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="text-zinc-300 hover:text-white transition-colors duration-200 flex items-center space-x-2"
+                  className="text-zinc-300 
+                    ease-in-out 
+                    flex items-center space-x-4 
+                    hover:text-zinc-50 
+                    hover:underline
+                    transition-transform duration-300 transform hover:scale-110"
                 >
-                  <span className="text-xl">{link.icon}</span>
-                  <ParagraphText text={link.text} className="" />
+                  <span className="text-xl ">{link.icon}</span>
+                  <ParagraphText text={link.text} />
                 </a>
               </li>
             ))}
