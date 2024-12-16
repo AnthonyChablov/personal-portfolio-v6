776dca5424d03f2c93c05d1689cf13b141f2b65c
Anthony Chablov <achablov@myseneca.ca>
2024-12-15 19:31:19 -0500

    refactor: file structure

---
+++ src/components/Layout/Separator.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { cn } from "@/lib/utils";
+
+interface SeparatorProps {
+  size?: "small" | "medium" | "large" | "extraLarge"; // Optional size prop
+  className?: string;
+}
+
+const Separator: React.FC<SeparatorProps> = ({
+  size = "small",
+  className = "",
+}) => {
+  // Map size to padding classes
+  const sizeClasses = {
+    small: "py-3",
+    medium: "py-6",
+    large: "py-9",
+    extraLarge: "py-20",
+  };
+
+  return <div className={cn(sizeClasses[size] + "" + className)}></div>;
+};
+
+export default Separator;
