0acadbb7da54935d1dc73e04003fb5dcf8ac3c56
Anthony Chablov <achablov@myseneca.ca>
2024-12-17 13:21:21 -0500

    feat: remove side navigation

--- src/components/Text/HeaderText.tsx
+++ src/components/Text/HeaderText.tsx
@@ -4,6 +4,7 @@ import { cn } from "@/lib/utils";
 interface HeaderTextProps {
   className?: string;
   header: string;
+  id?: string;
   headerLevel?: "h1" | "h2" | "h3" | "h4" | "h5";
 }
 
@@ -11,6 +12,7 @@ const HeaderText = ({
   className = "",
   header = "",
   headerLevel = "h1",
+  id,
 }: HeaderTextProps) => {
   const headerSizes = {
     h1: "text-6xl",
@@ -24,7 +26,10 @@ const HeaderText = ({
   const sizeClass = headerSizes[headerLevel];
 
   return (
-    <div className={cn(`text-zinc-200 dark:text-zinc-900 ${className}`)}>
+    <div
+      id={id || header}
+      className={cn(`text-zinc-200 dark:text-zinc-900 ${className}`)}
+    >
       <HeaderTag className={sizeClass}>{header}</HeaderTag>
     </div>
   );
