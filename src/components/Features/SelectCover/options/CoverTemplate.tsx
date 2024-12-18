014792f7df6cda25685a6be12e98511fc7c6f6df
Anthony Chablov <achablov@myseneca.ca>
2024-12-17 22:33:20 -0500

    feat: create reusable voer template

---
+++ src/components/Features/SelectCover/options/CoverTemplate.tsx
@@ -0,0 +1,27 @@
+"use client";
+import React, { ReactNode, CSSProperties } from "react";
+
+// Define the interface for props
+interface TemplateProps {
+  style?: CSSProperties; // Optional inline style prop
+  children?: ReactNode; // Optional children prop
+}
+
+const CoverTemplate: React.FC<TemplateProps> = ({ style, children }) => {
+  return (
+    <>
+      {/* Background Glitch Animation */}
+      <div
+        className="absolute top-0 left-0 w-full h-full overflow-hidden bg-black"
+        style={style}
+      >
+        {children}
+      </div>
+
+      {/* Keyframes */}
+      <style jsx>{``}</style>
+    </>
+  );
+};
+
+export default CoverTemplate;
