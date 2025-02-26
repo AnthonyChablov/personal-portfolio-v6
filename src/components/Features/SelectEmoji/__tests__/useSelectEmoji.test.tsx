2ec5de88cc14d001d20b87cd9cc5d66d314ce788
Anthony Chablov <achablov@myseneca.ca>
2025-02-25 23:26:20 -0500

    refactor: remove unneeded SelectEmoji hooks

--- src/components/Features/SelectEmoji/__tests__/useSelectEmoji.test.tsx
+++ src/components/Features/SelectEmoji/__tests__/useSelectEmoji.test.tsx
@@ -1,15 +1,8 @@
 import { describe, it, expect } from "vitest";
-import { filterEmojis } from "../hooks/useEmojiSearch";
 import { emojiList } from "../data/emojisListData";
 
 describe("filterEmojis", () => {
   it("should return the searched emoji", () => {
-    // Arrange
-    const searchTerm = "ghost";
-    // Act
-    const filteredEmojis = filterEmojis(emojiList, searchTerm);
-    // Assert
-    expect(filteredEmojis[0].emoji).toBe("👻");
-    expect(filteredEmojis[0].id).toBe(1);
+    
   });
 });
