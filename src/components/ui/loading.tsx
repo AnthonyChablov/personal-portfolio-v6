108c9e19b84c0afc9ba8d2d357d85daf43e33012
Anthony Chablov <achablov@myseneca.ca>
2025-04-06 13:30:07 -0400

    feat: adjust padding loader

--- src/components/ui/loading.tsx
+++ src/components/ui/loading.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Loading = () => {
   return (
-    <div className="flex items-center justify-center w-full h-full">
+    <div className="flex items-center justify-center w-fit h-fit py-6">
       <div
         className="
           w-12 h-12 
